9b7566a4af9b6b68a6cab7e7d8620f94d27ff162
Sean Koh <sean.koh.2015@smu.edu.sg>
2023-07-13 11:13:35 +0800

    Test if Standalone works with vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'standalone'
+  output: 'standalone', // comment if you need to deploy w/ Vercel 
 }
 
 module.exports = nextConfig
