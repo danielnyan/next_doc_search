838ef1636d02cfa437d16ed0248b19a62dbc9913
Daniel <e0235108@u.nus.edu>
2023-07-31 16:34:09 +0800

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,33 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const MainContent = () => (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <SearchDialog />
+      </div>
+
+      <div className="py-8 w-full flex items-center justify-center space-x-6">
+        <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+          <Link href="https://www.tech.gov.sg/capability-centre-dsaid/" className="flex items-center justify-center">
+            <p className="text-base mr-2">© 2023 Data Science and Artificial Intelligence Division, GovTech</p>
+            <Image src={'/dsaid.svg'} width="60" height="26" alt="DSAID logo" />
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+
+  const PasswordForm = ({ onSubmit }) => (
+    <div id="passwordForm">
+      <h2>Enter the Password to Access the Website:</h2>
+      <form onSubmit={onSubmit}>
+        <input type="password" name="passwordInput" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+  
   return (
     <>
       <Head>
@@ -19,20 +46,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <SearchDialog />
-        </div>
-
-        <div className="py-8 w-full flex items-center justify-center space-x-6">
-          <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-            <Link href="https://www.tech.gov.sg/capability-centre-dsaid/" className="flex items-center justify-center">
-              <p className="text-base mr-2">© 2023 Data Science and Artificial Intelligence Division, GovTech</p>
-              <Image src={'/dsaid.svg'} width="60" height="26" alt="DSAID logo" />
-            </Link>
-          </div>
-        </div>
-      </main>
+      <div>
+        {authenticated ? <MainContent /> : <PasswordForm onSubmit={handleSubmit} />}
+      </div>
     </>
   )
 }
