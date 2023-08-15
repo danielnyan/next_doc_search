d97534952391c6caab5c742b52adf3d97f119dde
Daniel <e0235108@u.nus.edu>
2023-08-15 22:47:58 +0800

    added codebase for matching content to links

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone', // comment if you need to deploy w/ Vercel 
-}
-
-module.exports = nextConfig
