ffffb933d1bda4302d9a162f7499fedc023092a1
Daniel <e0235108@u.nus.edu>
2023-09-18 22:42:24 +0800

    more bugfixing

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -6,10 +6,10 @@ import {geocode, GeocodeResult} from '@/lib/solar-estimator/geocode'
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  const {LAT: LAT, LONG: LONG} = await geocode("464, Pasir Ris Street 41", process.env.TOMTOM_API_KEY);
+  const {LAT: LAT, LON: LON} = await geocode("464, Pasir Ris Street 41", process.env.TOMTOM_API_KEY);
   const now = new Date();
   const DT = now.toISOString();
-  console.log(await getSunInfo(LAT, LONG, DT));
+  console.log(await getSunInfo(LAT, LON, DT));
   
   response.status(200).json({
     // body: request.body,
