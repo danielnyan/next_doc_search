74a933591cbedbc43ac44acc3ec12b28b4e1cb05
Daniel <e0235108@u.nus.edu>
2023-09-18 22:29:04 +0800

    removed extra bracket

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -6,7 +6,7 @@ import {geocode, GeocodeResult} from '@/lib/solar-estimator/geocode'
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  const {LAT: LAT, LONG: LONG} = await geocode("464, Pasir Ris Street 41", process.env.TOMTOM_API_KEY));
+  const {LAT: LAT, LONG: LONG} = await geocode("464, Pasir Ris Street 41", process.env.TOMTOM_API_KEY);
   const now = new Date();
   const DT = now.toISOString();
   console.log(await getSunInfo(LAT, LONG, DT));
